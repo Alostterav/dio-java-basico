efeb3732f0983b387fe42bff243e9d84584b23b2
Alostterav <alanvieirak7@gmail.com>
2024-08-15 11:34:54 -0300

    update: updating this code with new content

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,16 @@
 public class Operadores {
     public static void main(String[] args){
-        String nomeCompleto = "LINGUAGEM "+"JAVA";
-        System.out.println(nomeCompleto);
+        int saldo = 200;
+        int divida = 200;
+
+        String pagarDivida = saldo >= divida ? "Você pode pagar sua divida": "Você não tem saldo suficiente";
+
+        System.out.println(pagarDivida);
+        int numero1 = 1;
+        int numero2 = 2;
+        boolean simNao = numero1 == numero2;
+
+        System.out.println(simNao);
 
     }
 }
