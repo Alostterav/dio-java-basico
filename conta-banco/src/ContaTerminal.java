ce8efa4f797cf8fa1322186b0d75d39e5231f84f
Alostterav <alanvieirak7@gmail.com>
2024-08-25 16:43:26 -0300

    meu codigo terminado da conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("numero da conta (dddd): ");
+        numeroConta = entrada.nextInt();
+
+        System.out.println("numero da agencia (ddd-d): ");
+        agencia = entrada.next();
+
+        System.out.println("nome do cliente: ");
+        nomeCliente = entrada.next();
+
+        System.out.println("saldo: ");
+        saldo = entrada.nextDouble();
+
+        String mensagem = String.format("Nome: %s\n Saldo: %.2f\n Numero da Conta: %d\n Agência: %s",
+        nomeCliente,saldo,
+        numeroConta,agencia);
+        
+        entrada.close();
+
+        System.out.print(mensagem);
+
+
+        
     }
 }
