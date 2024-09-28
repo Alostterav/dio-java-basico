f936c7efb615cd2622e1363d5d46352ad1818b06
Alostterav <alanvieirak7@gmail.com>
2024-09-28 12:47:10 -0300

    adicionando o diagrama

---
+++ Modelando_Iphone/src/Iphone.java
@@ -0,0 +1,14 @@
+import funcionalidades.*;
+
+public class Iphone {
+    public static void main(String[] args) {
+        // ReprodutorMusical playMusica = new ReprodutorMusical();
+        // playMusica.inciarReprodutorMusical();
+
+        AparelhoTelefonico aparelho = new AparelhoTelefonico();
+        // aparelho.ligar("21998931681");
+        aparelho.atender("21998931681");
+
+
+    }
+}
